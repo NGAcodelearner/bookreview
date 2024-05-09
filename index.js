bc191848750cda8fc295e3c5260bdebd0e5b9f8f
NGA <r0917268@student.thomasmore.be>
2024-05-09 13:17:56 +0700

    authentication mechanism

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
+
 const app = express();
 
-app.listen(5000, () => console.log(`Server listening on port 5000`));
+app.use(express.json());
+
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use("/customer/auth/*", function auth(req, res, next) {
+  //get the authorization details from the session and verifies it.
+  if (req.session.authorization) {
+    token = req.session.authorization["accesstoken"];
+    jwt.verify(token, "access", (error, user) => {
+      if (!error) {
+        req.user = user;
+        next();
+      } else {
+        return res.status(403).json({ message: "User is not authenticated" });
+      }
+    });
+  } else {
+    return res.status(403).json({ message: "User not logged in" });
+  }
+});
+
+const PORT = 8000;
+
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
+
+app.listen(PORT, () => console.log("Server is running"));
